51fa690c5fd4eaeb5a68279ab7d6aaaceb57a642
blackberry-web <maryananakonechna123@gmail.com>
2024-12-12 21:24:44 +0200

    fix: env

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    baseUrl: 'https://opentdb.com',
+  },
 };
 
 export default nextConfig;
--- src/app/lib/common.ts
+++ src/app/lib/common.ts
@@ -1,9 +1,8 @@
 import axios from 'axios';
 import { Question } from '../types/types';
-import 'dotenv/config';
 
 export const generateToken = async (): Promise<string> => {
-    const response = await axios.get(`${process.env.API_URL}/api_token.php?command=request`)
+    const response = await axios.get(`${process.env.baseUrl}/api_token.php?command=request`)
         .then((response) => response.data)
         .catch((error) => {
             throw new Error(error);
@@ -13,7 +12,7 @@ export const generateToken = async (): Promise<string> => {
 
 export const generateQuizQuestions = async (): Promise<Question[]> => {
     const token = await generateToken();
-    const questions = await axios.get(`${process.env.API_URL}/api.php?amount=10&category=18&type=multiple&token=${token}`)
+    const questions = await axios.get(`${process.env.baseUrl}/api.php?amount=10&category=18&type=multiple&token=${token}`)
         .then((response) => response.data)
         .catch((error) => {
             throw new Error(error);
